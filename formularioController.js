2a1afae3c548def29cf134a619d8727a02147fc1
Luciana Albuquerque <albuquerqueluciana54@gmail.com>
2017-09-25 03:07:00 -0300

    Completando formularioController

--- formularioController.js
+++ formularioController.js
@@ -1,3 +1,22 @@
 function formularioController(){
 
+  this.formMed = document.getElementById('formMed');
+  this.inputNome = document.getElementById('nome');
+  this.inputRegistro = document.getElementById('numeroRegistro');
+  this.inputControlado = document.getElementById('controlado');
+  this.inputCategoria = document.getElementById('categoria');
+  this.btnCadastrar = document.getElementById('btnCadastrar');
+  this.btnLimpar = document.getElementById('btnLimpar');
+
+  this.row = null;
+
+  this.dados = function(){
+    var controlado = document.querySelector('input[name="controlado"]:checked');
+    return {
+      nome: this.inputNome.value,
+      registro: this.inputRegistro.value,
+      controlado: controlado ? controlado.value : '',
+      categoria: this.inputCategoria.value
+    };
+  };
 }
